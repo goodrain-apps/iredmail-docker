f907a8772b81862bd3925e1199a284c42965b95b
ysicing <root@ysicing.net>
2018-09-12 16:39:49 +0800

    add debug

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker build --no-cache -t goodrainapps/iredmail:0.9.8 mysql
 
 docker run --privileged -p 80:80 -p 443:443 \
-           -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
+           -e "DOMAIN=install.dog" -e "HOSTNAME=mail" \
            -e "SOGO_WORKERS=2" \
            -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
            -e "TIMEZONE=Asia/Shanghai" \
